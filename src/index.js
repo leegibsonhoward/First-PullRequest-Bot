ee4033134cb5b17b7559ce0a45fe579385b7d3e0
leeghwrd <lee.develops@icloud.com>
2019-08-15 12:34:27 -0700

    created client and added token.

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+require('dotenv').config();
+
+// new client
+const client = new Discord.Client();
+
+// message to client.
+
+// check for messages.
+
+// login client using token
+client.login(process.env.TOKEN);
